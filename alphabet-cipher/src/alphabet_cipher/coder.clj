fc29cb9f9324dec62673d09f9a003235db3f9dd9
Martin <martin_poropat@yahoo.com.ar>
2016-05-03 20:18:34 -0300

    Enconde alphabet-cipher

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,34 @@
 (ns alphabet-cipher.coder)
+(require '[clojure.string :as str])
+
+(def abc "abcdefghijklmnopqrstuvwxyz")
+
+(defn getline [letter]
+  (subs
+   (apply str
+          (take
+           (* 2 (count abc))
+           (cycle abc))
+          )
+   (str/index-of abc letter)
+   (+ (str/index-of abc letter) (count abc)))
+  )
 
 (defn encode [keyword message]
-  "encodeme")
+  (let [keywordcycled (apply str (take (count message) (cycle keyword)))]
+    (apply str
+           (map
+            #(get (getline (str %2)) (str/index-of abc %1))
+            keywordcycled
+            message)
+           )
+    )
+  )
+
+(encode "scones" "meetmebythetree")
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-

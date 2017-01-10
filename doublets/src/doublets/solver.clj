fba14793542ee02c0bd0abbdfcb37b47e1267ff7
Martin <martin_poropat@yahoo.com.ar>
2017-01-09 22:05:32 -0300

    doublets

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -1,11 +1,36 @@
 (ns doublets.solver
-  (:require [clojure.java.io :as io]
-            [clojure.edn :as edn]))
+  (:require [clojure.java.io :as io]))
 
 (def words (-> "words.edn"
                (io/resource)
                (slurp)
                (read-string)))
 
-(defn doublets [word1 word2]
-  "make me work")
+; counts differences between 2 words
+(defn differences [word1 word2]
+  (let [extra (Math/abs (- (count word1) (count word2)))
+        comparable (map #(= %1 %2) word1 word2)]
+    (+ extra (reduce
+          (fn [dif w] (if (false? w) (inc dif) dif))
+          0
+          comparable))))
+
+; brings words with only 1 letter change (excludes a list of words previously defined)
+(defn mutate
+  ([word] (mutate word []))
+  ([word excludes]
+   (remove (fn [x] (some #(= x %) excludes))
+            (reduce (fn [r w] (if (= (differences word w) 1) (conj r w) r))
+                    #{}
+                    words))))
+
+(defn doublets
+  ([word1 word2] (doublets word1 word2 (into [] (vector word1))))
+  ([word1 word2 solver]
+   (if (= word2 (last solver))
+     solver
+     (let [n (first (for [mutated-word (mutate (last solver) solver)
+                          :let [l (doublets word1 word2 (into solver (vector mutated-word)))]
+                          :when (not (empty? l))]
+                      l))]
+       (if (nil? n) [] n)))))
--- fox-goose-bag-of-corn-logic/src/fox_goose_bag_of_corn_logic/core.clj
+++ fox-goose-bag-of-corn-logic/src/fox_goose_bag_of_corn_logic/core.clj
@@ -2,8 +2,85 @@
 
 (use 'clojure.core.logic)
 
-(run* [q]
-  (== q true))
+(defne not-membero [x l]
+  ([_ []])
+  ([_ [?y . ?r]]
+    (!= x ?y)
+    (not-membero x ?r)))
 
+(defne move [state after]
+  ; Move from Left to center with sth
+  ([[[a b c :you] [:boat] t]
+    [[a b] [:boat :you c] t]] (== [a b] [:corn :fox]))
+  ([[[a b c :you] [:boat] t]
+    [[a c] [:boat :you b] t]] (== [a c] [:corn :fox]))
+  ([[[a b c :you] [:boat] t]
+    [[b c] [:boat :you a] t]] (== [b c] [:corn :fox]))
+  ([[[a b :you] [:boat] t]
+    [[a] [:boat :you b] t]])
+  ([[[a b :you] [:boat] t]
+    [[b] [:boat :you a] t]])
+  ([[[a :you] [:boat] t]
+    [[] [:boat :you a] t]])
+  ; Move from Left to Center alone
+  ([[[a b :you] [:boat] t]
+    [[a b] [:boat :you] t]]  (== [a b] [:corn :fox]))
+  ([[[a :you] [:boat] t]
+    [[a] [:boat :you] t]])
+  ; Move from Right to center with sth
+  ([[t [:boat] [a b :you]]
+    [t [:boat :you a] [b]]])
+  ([[t [:boat] [a b :you]]
+    [t [:boat :you b] [a]]])
+  ([[t [:boat] [a :you]]
+    [t [:boat :you a] []]])
+  ; Move from Right to Center alone
+  ([[t [:boat] [a b :you]]
+    [t [:boat :you] [a b]]] (== [a b] [:corn :fox]))
+  ([[t [:boat] [a :you]]
+    [t [:boat :you] [a]]])
+  ; Move from Center to Left with sth
+  ([[[b] [:boat :you a] u]
+    [[b a :you] [:boat] u]])
+  ([[[] [:boat :you a] u]
+    [[a :you] [:boat] u]])
+  ([[[b] [:boat :you a] u]
+    [[b a :you] [:boat] u]])
+  ; Move from Center to Left alone  
+  ([[[a b] [:boat :you] u]
+    [[a b :you] [:boat] u]])
+  ([[[a] [:boat :you] u]
+    [[a :you] [:boat] u]])
+  ([[[] [:boat :you] u]
+    [[:you] [:boat] u]])
+  ; Move from Center to Right with sth
+  ([[u [:boat :you a] [b]]
+    [u [:boat] [b a :you]]])
+  ([[u [:boat :you a] []]
+    [u [:boat] [a :you]]])
+  ([[u [:boat :you a] [b]]
+    [u [:boat] [b a :you]]])
+  ([[u [:boat :you a] [b c]]
+    [u [:boat] [b c a :you]]])
+  ; Move from Center to Right alone
+  ([[u [:boat :you] [a b]]
+    [u [:boat] [a b :you]]])
+  ([[u [:boat :you] [a]]
+    [u [:boat] [a :you]]])
+  ([[u [:boat :you] []]
+    [u [:boat] [:you]]])
+  )
+  
+(defne river-crossing-plan [state out plan]
+  ([[[] [:boat] [_ _ _ :you]] plan])
+  ([[_ _ _] _] (fresh [next-state new-plan]
+                 (move state next-state)
+                 (not-membero next-state plan)
+                 (appendo plan [next-state] new-plan)
+                 (river-crossing-plan next-state out new-plan))))
 
+; You must get the fox, goose, and bag of corn safely across the other side of the river
+(def start-step [[:corn :goose :fox :you] [:boat] []])
 
+(let [a (run* [q] (river-crossing-plan start-step q [start-step]))]
+  a)
